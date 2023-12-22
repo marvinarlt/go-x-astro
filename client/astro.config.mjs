77ebd987e2f7b171dbae48b73f969bddfccca83f
Marvin Arlt <marvinarlt@gmx.net>
2023-12-22 23:25:14 +0100

    Changed build dir

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 /** @type {import('astor/config').AstroUserConfig} */
 export default defineConfig({
+  outDir: './../dist',
   build: {
     format: 'file',
   },
